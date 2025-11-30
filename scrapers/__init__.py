d5b6a58020d828ee26aa6d7a36d91b7ded8bc9ac
Porcius13 <tilsimsticker@gmail.com>
2025-11-30 20:41:37 +0300

    fix: scrapers/__init__.py import hatası düzeltildi - eksik site_specific modülü kaldırıldı

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -3,8 +3,8 @@ Scrapers Module
 Site-specific scraping logic organized by domain
 """
 
-from scrapers.site_specific import SiteSpecificExtractor
-from scrapers.config import SITE_CONFIGS, get_site_config
+from scrapers.config import SITE_CONFIGS, get_site_config, get_timeout
+from scrapers.utils import format_price, extract_price_from_text, normalize_image_url, should_skip_image
 
-__all__ = ['SiteSpecificExtractor', 'SITE_CONFIGS', 'get_site_config']
+__all__ = ['SITE_CONFIGS', 'get_site_config', 'get_timeout', 'format_price', 'extract_price_from_text', 'normalize_image_url', 'should_skip_image']
 
